1166a6dcb08848e37036c06a961e94c7ca2fb04b
pkc-3 <pkc_3@naver.com>
2021-05-20 11:30:53 +0900

    2021/05/18 Java Ch03 수업 예제

---
+++ Ch03/src/sub2/ForTest.java
@@ -0,0 +1,67 @@
+package sub2;
+
+import java.util.Iterator;
+
+/*
+ * 날짜 : 2021/05/18
+ * 이름 : 고현석
+ * 내용 : 자바 for 반복문 실습하기
+ * 
+ */
+public class ForTest {
+	public static void main(String[] args) {
+		// for
+		for (int i=1 ; i<= 3 ; i++) {
+			System.out.printf("%d.Hello Java! \n", i);
+		}
+		// 1부터 10까지 합
+		int sum = 0;
+		for (int k=1; k <=10 ; k++) {
+			//sum = sum + k;
+			sum += k;
+		}
+		System.out.println("1부터 10까지 합 :"+sum);
+		// 1부터 10까지 짝수합
+		int eSum = 0;
+		for (int k=1; k<=10;k++) {
+			if(k%2==0) {
+				eSum += k;
+			}
+		}
+		System.out.println("1부터 10까지 짝수 합 :"+eSum);
+		// 중첩for
+		for (int a=1;a<=3;a++) {
+			System.out.println("a :"+a+"라운드");
+			for(int b=1;b<=5;b++) {
+				System.out.println("b :"+b);
+			}
+		}
+		// 구구단
+		for(int x=2;x<=9;x++) {
+			System.out.println(x+"단");
+			for(int y=1;y<=9;y++) {
+				//System.out.println(x+" X "+ y +" : " +(x*y));
+				int z = x*y;
+				System.out.printf("%d X %d = %d\n", x,y,z);
+			}
+		}
+		
+		// 별삼각형1
+		for (int start=1;start<=10;start++) {
+			for(int end=1;end<=start;end++) {
+				System.out.print("☆");
+			}
+			System.out.print("\n");
+		}
+		// 별삼각형2
+		for(int a=1; a<=5;a++) {
+			for(int b=1;b<=5-a;b++) {
+				System.out.print("☆");
+			}
+			for(int c=1;c<=a;c++) {
+				System.out.print("★");
+			}
+			System.out.print("\n");
+		}
+	}
+}
---
+++ Ch03/src/sub3/ArrayTest.java
@@ -0,0 +1,32 @@
+package sub3;
+/*
+ * 날짜 : 2021/05/18
+ * 이름 : 고현석
+ * 내용 : 자바 Array 배열 실습하기
+ * 
+ */
+public class ArrayTest {
+	public static void main(String[] args) {
+		//배열 java에 배열은 python의 tuple과 비슷해 크기 늘이거나 줄이기 불가능(정적 리스트) <-> arraylist(동적 리스트)
+		int[] nums = {1,2,3,4,5};
+		
+		//배열 길이
+		System.out.println("배열 nums의 원소의 갯수 : "+nums.length);
+		
+		//배열 원소 출력
+		System.out.println("배열 nums의 1번째 원소 : "+nums[0]);
+		System.out.println("배열 nums의 2번째 원소 : "+nums[1]);
+		System.out.println("배열 nums의 3번째 원소 : "+nums[2]);
+		
+		//배열 반복문
+		for(int num : nums) {
+			System.out.println("배열 nums의 원소 : "+num);
+		}
+		//문자열 배열
+		String[] people = {"김유신","김춘추","장보고","강감찬","이순신"};
+		
+		for(String person : people) {
+			System.out.println("people 원소 : "+person);
+		}
+	}
+}
